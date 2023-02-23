943c61f027672c5acbb4e1709cd1304dc5c919ca
Dung BV <dung.bv@teko.vn>
2023-02-23 15:11:33 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,13 +13,22 @@ with open(os.path.join(HERE, 'README.md'), 'r', encoding='utf8') as f:
 
 setup(
     name='yasuo',
-    version='1.0.2',
+    version='2.0.2',
     description='A Yasuo Python package',
     long_description=long_description,
     long_description_content_type='text/markdown',
     author='Dung BV',
+    url='https://github.com/puffer-python/yasuo',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    license='MIT License',
+    keywords='yasuo,python,vietnamese',
     author_email='bvdzung@gmail.com',
     packages=['yasuo'],
+    platforms=['Linux (x86, x86_64, ARMv6, ARMv7, ARMv8)', 'Windows (32-bit, 64-bit)', 'macOS (x86_64)'],
     install_requires=[
         'pytz', 'slugify', 'unidecode'
     ],
